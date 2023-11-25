aea0e3b2e7f38d4eb5729322f0c3d722274556f0
Joao Paulo Pacheco <jppacheco09@hotmail.com>
2023-11-25 16:17:25 -0300

    feat: change env var

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -38,7 +38,8 @@ const Home = () => {
 
   useEffect(() => {
     getData();
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onChangeSearch = (
     event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
